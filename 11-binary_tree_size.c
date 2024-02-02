dccc29cc105326abb276fce2504b9026781e7ce5
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-02-01 16:04:13 -0800

    whole

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the size
+ *
+ * Return: Size of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_s, right_s;
+
+	if (tree == NULL)
+		return (0);
+
+	left_s = binary_tree_size(tree->left);
+	right_s = binary_tree_size(tree->right);
+
+	return (left_s + right_s + 1);
+}
